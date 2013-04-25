f97926f215aba7d1b4ae2b05da03fb4449e7da06
joe <joe.pinsonault@gmail.com>
2013-04-25 14:32:07 -0700

    Moved defines to new file

--- chatserver.c
+++ chatserver.c
@@ -1,4 +1,3 @@
-
 //----- Include files ---------------------------------------------------------
 #include <stdio.h>          // for printf()
 #include <stdlib.h>         // for exit()
@@ -17,24 +16,10 @@
 #include <signal.h>         /* for signal                     */ 
 #include <semaphore.h>      /* for p-thread semaphores        */
 /* ------------------------------------------------------------------------ */ 
+ 
 
 #include "chatfunctions.h"
- 
-//----- HTTP response messages ----------------------------------------------
-#define OK_IMAGE    "HTTP/1.0 200 OK\nContent-Type:image/gif\n\n"
-#define OK_TEXT     "HTTP/1.0 200 OK\nContent-Type:text/html\n\n"
-#define NOTOK_404   "HTTP/1.0 404 Not Found\nContent-Type:text/html\n\n"
-#define MESS_404    "<html><body><h1>FILE NOT FOUND</h1></body></html>"
- 
-//----- Defines -------------------------------------------------------------
-#define PORT_NUM            6110     // Port number for a Web server (TCP 5080)
-#define PEND_CONNECTIONS     100     // pending connections to hold 
-#define TRUE                   1
-#define FALSE                  0
-#define NTHREADS 5                     /* Number of child threads        */ 
-#define NUM_LOOPS  10                  /* Number of local loops          */
-#define SCHED_INTVL 5                  /* thread scheduling interval     */
-#define HIGHPRIORITY 10
+#include "chatserver.h"
  
 /* global variables ---------------------------------------------------- */
 sem_t thread_sem[NTHREADS];  
